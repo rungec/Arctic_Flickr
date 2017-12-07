8805e2b11dbc9d760464919265e7ae11dedc07b7
rungec <c.runge1@uq.edu.au>
2017-12-06 17:05:16 -0800

    calculate how many points per country

--- flickr.r
+++ flickr.r
@@ -10,6 +10,7 @@ setwd(wd)
 library(readr)
 library(rgdal)
 library(sf)
+library(zoo) #as.yearmon
 library(plyr)
 library(maps)
 library(animation)
@@ -28,6 +29,10 @@ dat <- read_csv("D:/Box Sync/Arctic/Data/Flickr/FlickrPhotosNorthOf60.csv")
 dat <- dat[dat$latitude >= 60, ] 
 #problems(dat) #check if load issues
 #spec(dat) #check column formats
+#add time rows
+dat$month <- format(dat$datetaken,"%m")
+dat$year <- format(dat$datetaken,"%Y")
+dat$yearmon <- format(dat$datetaken,"%Y%m")
 
 
 #load country borders shapefile
@@ -44,7 +49,8 @@ worldmap <- read_sf("D:/Box Sync/Arctic/Data/Boundaries/Arctic_circle/60degreesN
 subdat <- dat[1:10000,]
 simple.sf <- st_as_sf(subdat, coords=c('longitude', 'latitude'))
 st_crs(simple.sf) <- 4326 #WGS84
-
+#transform to north pole lambert
+simple.sf2 <- st_transform(simple.sf, crs=102017)
 
 #full dataset
 all.sf <- st_as_sf(dat, coords=c('longitude', 'latitude'))
@@ -57,23 +63,24 @@ st_crs(all.sf) <- 4326 #WGS84
 simple.sf %>% ggplot() + geom_sf(aes(size=5)) +
 	coord_sf(crs = st_crs(102017)) #plot in north pole lambert
 
-#Plot full dataset
-all.sf %>% ggplot() + geom_sf(size=1, alpha=0.7, colour="#fceccf") +
-	coord_sf(crs = st_crs(102017)) #plot in north pole lambert
-	ggsave(paste0("Flickr_60N_allpoints.png"))
+#Plot full dataset #this takes about a day
+#all.sf %>% ggplot() + geom_sf(size=1, alpha=0.7, colour="#fceccf") +
+	#coord_sf(crs = st_crs(102017)) #plot in north pole lambert
+#	ggsave(paste0("Flickr_60N_allpoints.png"))
 
 ############################
 #Time series
 ############################
 #How many photos each month/year
+counts <- ddply(dat,.(yearmon, month,year),nrow)
+countsub <- subset(counts, year <2018 & year > 1996)
+#fill in missing dates
+countsub <- rbind(countsub, c(199804, "04", 1998, 0))
+countsub <- countsub[with(countsub, order(countsub$yearmon)),]
 
-timedf <- transform(dat, month = format(datetaken,"%m"), year = format(datetaken, "%Y"), yearmon = format(datetaken, "%Y%m"))
-
-counts <- ddply(timedf,.(yearmon, month,year),nrow)
-countsub <- subset(counts, year <2018 & year > 1996)	
 a<-ts(countsub$V1,start=c(1997,1),freq=12)
 #print(a)
-write.csv(matrix(c(a, 0, 0), ncol=12, byrow=TRUE, dimnames=list(c(1997:2017), format(seq.Date(as.Date('2000-01-01'), by = 'month', len = 12), "%b"))), "Flickr_60N_numberofpoints_byyearmon.csv", row.names=TRUE)
+write.csv(matrix(c(a, 0), ncol=12, byrow=TRUE, dimnames=list(c(1997:2017), format(seq.Date(as.Date('2000-01-01'), by = 'month', len = 12), "%b"))), "Flickr_60N_numberofpoints_byyearmon.csv", row.names=TRUE)
 
 png(paste0(wd, "/Timeseries_flickr_allarticphotos.png"),width=1000, height=400) 
 	plot(a, type="l", lwd=2, col="red", ylab= "Number of photos",xlim=c(2004,2017),axes=F)
@@ -86,9 +93,63 @@ dev.off()
 ############################
 #Summarise by country
 ############################
+#Which points fall within which country
+spatialjoin <- st_intersects(simple.sf2, worldmap, sparse = FALSE)
+spatialjoin <- cbind(spatialjoin, apply(spatialjoin, 1, function(x) all(x==FALSE)))
+dimnames(spatialjoin) <- list(NULL, c(worldmap$ADMIN[1:10], "Alaska", "Marine"))
+all.sf <- cbind(all.sf, spatialjoin)
+
+#Summarise
+timeList <- lapply(c(worldmap$ADMIN[1:10], "Alaska", "Marine"), function(region) {
+    sub.sf <- all.sf[match.fun('==')(all.sf[[region]], TRUE), ] #select points in region
+    #if(nrow(sub.sf)>0) {
+      counts <- ddply(sub.sf, .(yearmon, month, year), nrow)
+      countsub <- subset(counts, year <2018 & year > 1996)
+    #} else {
+    #countsub <- 0
+    #}
+    return(countsub)
+})
 
 
 
+all.sf$month <- format(all.sf$datetaken,"%m")
+all.sf$year <- format(all.sf$datetaken,"%Y")
+all.sf$yearmon <- format(all.sf$datetaken,"%Y%m")
+
+
+
+timeDF <- do.call(merge, timeList)
+?or merge
+func <- function(x,y){merge(x, y, by.x=names(x)[1], by.y=names(y)[1])}
+lapply(list.df, func, df)
+
+cbind.fill <- function(...){
+  nm <- list(...) 
+  nm <- lapply(nm, as.matrix)
+  n <- max(sapply(nm, nrow)) 
+  do.call(cbind, lapply(nm, function (x) 
+    rbind(x, matrix(, n-nrow(x), ncol(x))))) 
+}
+
+s <- seq(as.yearmon(as.character(199701), "%Y%m"), as.yearmon(as.character(201711), "%Y%m"), 1/12) # create yearmon sequence
+as.numeric(format(s, "%Y%m")) # convert to numeric yyyymm
+
+#fill in missing dates
+
+
+#save
+write.csv(matrix(c(a, 0), ncol=12, byrow=TRUE, dimnames=list(c(1997:2017), format(seq.Date(as.Date('2000-01-01'), by = 'month', len = 12), "%b"))), "Flickr_60N_numberofpoints_byyearmon.csv", row.names=TRUE)
+
+png(paste0(wd, "/xxxx.png"),width=1000, height=400) 
+library(ggplot2)
+library(reshape2)
+
+df_melt = melt(df, id.vars = 'date')
+ggplot(df_melt, aes(x = date, y = value)) + 
+  geom_line() + 
+  facet_wrap(~ variable, scales = 'free_y', ncol = 1)
+dev.off()
 
 
 
@@ -150,4 +211,4 @@ graphics.off()
 
 #run across months		 
 saveHTML(FUN2(timeList=c(1:12), timevar="month"), img.name="Flickr_60N_byMonth", imgdir="animation_images", htmlfile="Flickr_60N_byMonth.html", autoplay = FALSE, loop = FALSE, verbose = FALSE, single.opts = "'controls': ['first', 'previous', 'play', 'next', 'last', 'loop', 'speed'], 'delayMin': 0")
-
+graphics.off()
