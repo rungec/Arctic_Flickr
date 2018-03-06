bc4d8dab26f91c834d6ee10deee7364b70961e89
Claire Runge <cru016@uit.no>
2018-03-06 17:32:03 +0100

    added more stopwords, and added more punctuation characters to drop list

--- Flickr_tidy_flickrtags.R
+++ Flickr_tidy_flickrtags.R
@@ -17,7 +17,7 @@ options(tibble.width = Inf) #print all columns
 
 ### Load data ----
 flickrshp <- read_sf("D:/Box Sync/Arctic/Data/Flickr/Flickr_Artic_60N_byregion_laea_icelandupdate.shp")
-#write.csv(data.frame(flickrshp$tags, flickrshp$title, flickrshp$url_m), "/tag_analysis/input/flickr_tags_and_titles_all_photos.csv")
+#write.csv(data.frame(flickrshp$tags, flickrshp$title, flickrshp$url_m), "tag_analysis/input/flickr_tags_and_titles_all_photos.csv")
 
 length(unique(flickrshp$title))
 length(unique(flickrshp$tags))
@@ -31,21 +31,36 @@ titles <- flickrshp$title
 ### Tidy up data ----
 
 #Tidy tags
-taglist <- str_split(tags, pattern=" ", simplify=FALSE) #drop spaces
+
+taglist <- str_replace_all(tags, "[\\$+^'~|¨¦´£¤°±×=¬¥!@#%&*()_+:\"?,./;'-{}]", "") #get rid of punctuation, but not : or =
+taglist <- str_replace_all(taglist, "[\\>]", " ") #replace > or < with space
+taglist <- str_split(taglist, pattern=" ", simplify=FALSE) #drop spaces
 taglist <- lapply(taglist, function(currtags) {
   newtags <- currtags[which(str_detect(currtags, "[0-9]")==FALSE)]
+  newtags <- newtags[which(str_detect(newtags, "[\\©]")==FALSE)] #drop any words containing ©
+  newtags <- newtags[which(str_detect(newtags, "copyright")==FALSE)] #drop any words containing ©
+  newtags <- newtags[which(str_detect(newtags, "exif")==FALSE)] #drop any words containing exif
+  newtags <- newtags[which(str_length(newtags)>0)] #drop any blank entries
 }) #drop any words containing numbers
 
 #Tidy titles
-stopwords <- c("and", "this", "the", "of", "a") #define stopwords
-titlelist <- str_replace_all(titles, "[[:punct:]]", "") #get rid of punctuation
+stopwords <- c("and", "this", "the", "of", "a", "in", "at", "on", "to", "from", "i", "for", "with", "de", "la", "is", "på", "by", "my", "og", "vs", "en", "it", "up", "you", "near", "an", "one", "our", "med") #define stopwords
+titlelist <- str_replace_all(titles, "[\\$+^'~|¨¦´£¤°±×=¬¥!@#%&*()_+:\"?,./;'-{}]", "") #get rid of punctuation, but not : or =
+titlelist <- str_replace_all(titlelist, "[\\>]", " ") #replace > or < with space
 titlelist <- tolower(titlelist) #lowercase
 titlelist <- str_split(titlelist, pattern=" ", simplify=FALSE) #each word becomes an item in a vector, drop spaces
 titlelist <- lapply(titlelist, function(currtitles){
   newtitles <- currtitles[which(str_detect(currtitles, "[0-9]")==FALSE)] #drop any words containing numbers
+  newtitles <- newtitles[which(str_detect(newtitles, "[\\©]")==FALSE)] #drop any words containing ©
+  newtitles <- newtitles[which(str_detect(newtitles, "copyright")==FALSE)] #drop any words containing ©
+  newtitles <- newtitles[which(str_detect(newtitles, "exif")==FALSE)] #drop any words containing exif
   newtitles <- newtitles[!newtitles %in% stopwords] #drop stopwords 
   newtitles <- newtitles[which(str_length(newtitles)>0)] #drop any blank entries
   }) 
+
+
+#if you want to change åøæ etc 
+#iconv(x, to = "ASCII//TRANSLIT")
 
 ### Calculate stats on data ----
 
@@ -57,11 +72,11 @@ length(unique(unlist(titlelist)))
 
 #frequency table of tags
 freq_tags <- plyr::count(unlist(taglist))
-write.csv(freq_tags, "/tag_analysis/output/frequency_of_flickr_tag_words.csv")
+write.csv(freq_tags, "tag_analysis/output/frequency_of_flickr_tag_words.csv")
 
 #frequency table of titles
 freq_titles <- plyr::count(unlist(titlelist))
-write.csv(freq_titles, "/tag_analysis/output/frequency_of_flickr_title_words.csv")
+write.csv(freq_titles, "tag_analysis/output/frequency_of_flickr_title_words.csv")
 
 
 
