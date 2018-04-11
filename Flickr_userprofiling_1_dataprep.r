daf46fcfc7ed75ef45ad1fd43846891f42cd6496
rungec <runge@nceas.ucsb.edu>
2018-04-11 15:10:01 +0200

    changed the way ranging distances are calculated, updated ttest to wilcox test, added IQR summaries

--- Flickr_userprofiling_1_dataprep.r
+++ Flickr_userprofiling_1_dataprep.r
@@ -59,7 +59,9 @@ testusers <- userfreq[userfreq$n==1 | userfreq$n==2, ] #test users 1 or 2 photos
 allowners <- unique(flickrshp$owner)
 
 allownerstats <- lapply(allowners, function(currowner){
+  #extract subset for currowner
   currphotos <- flickrshp[flickrshp$owner==currowner, ]
+  
   #how long between first and last photos
   totaltrip_days <- as.numeric(difftime(max(currphotos$datetkn), min(currphotos$datetkn), units="days"))
 
@@ -74,6 +76,15 @@ allownerstats <- lapply(allowners, function(currowner){
   #reorder photos by date, add distance per day & time between consecutive photos
   currphotos <-  cbind(currphotos[bydate,], dists, timebtwphotos)
   total_trip_dist <- sum(currphotos$dists)
+  
+  #what is the centroid of the convex hull bounding their photos
+  user_centroid <- st_geometry(currphotos) %>% st_union() %>% st_convex_hull() %>% st_centroid()
+  
+  #furthest & average distance travelled (in km) from this centroid
+  distances <- currphotos %>% st_distance(user_centroid)
+  maxdistfromcentroid_km <- max(distances)/1000
+  avgdistfromcentroid_km <- mean(distances)/1000
+  
   #how many trips did they take, defining a trip as any photos separated by 14 days or more
   counter=1
   currphotos$tripid <- NA
@@ -81,10 +92,11 @@ allownerstats <- lapply(allowners, function(currowner){
         if(currphotos$timebtwphotos[i] >= 14) counter=counter+1 
         currphotos$tripid[i] <- counter
   }
-  #stats for each trip
+  
   #total number of trips
   numtrips_overall <- max(currphotos$tripid)
   numphotos_overall <- nrow(currphotos)
+  
   #total distance travelled each trip
   tripstats <- currphotos %>% st_set_geometry(NULL) %>% group_by(tripid) %>% 
                                          summarise(tripdist_km=sum(dists),
@@ -96,13 +108,6 @@ allownerstats <- lapply(allowners, function(currowner){
                           #         summarise(tripdist_avperday_km=mean(dailydist),
                            #                 tripdist_medianperday_km=median(dailydist))
   
-  #what is the centroid of the convex hull bounding their photos
-  user_centroid <- st_geometry(currphotos) %>% st_union() %>% st_convex_hull() %>% st_centroid()
-  
-  #furtherest & average distance travelled (in km) from this centroid
-  distances <- currphotos %>% st_distance(user_centroid)
-  maxdistfromcentroid_km <- max(distances)/1000
-  avgdistfromcentroid_km <- mean(distances)/1000
   #where is the centroid of each trip, and how far is it from the total centroid?
   tripstats2 <- data.frame()
   trip_centroids <- c()
@@ -110,23 +115,34 @@ allownerstats <- lapply(allowners, function(currowner){
     currtrip <- currphotos[currphotos$tripid==currid,  ]
     numphotos_trip <- nrow(currtrip)
     currcentroid <- currtrip %>% st_union() %>% st_convex_hull() %>% st_centroid()
-    avgtripdist_from_centroid <- currcentroid %>% st_distance(user_centroid)/1000
+	distance_from_centroid <- currcentroids %>% st_distance(user_centroid)/1000
+    avgtripdist_from_centroid <- mean(currtrip %>% st_distance(curr_centroid)/1000)
     maxtripdist_from_centroid <- max(currtrip %>% st_distance(currcentroid)/1000)
-   tripstats2 <- rbind(tripstats2, c(maxtripdist_from_centroid[[1]], avgtripdist_from_centroid[[1]], numphotos_trip)) 
+   tripstats2 <- rbind(tripstats2, c(distance_from_centroid[[1]], maxtripdist_from_centroid[[1]], avgtripdist_from_centroid[[1]], numphotos_trip)) 
    trip_centroids <- rbind(trip_centroids, st_coordinates(currcentroid))
   }
-  names(tripstats2) <- c("maxtripdist_from_centroid", "avgtripdist_from_centroid", "numphotos_trip")
+  names(tripstats2) <- c("tripcentroid_distance_from_usercentroid", "maxtripdist_from_centroid", "avgtripdist_from_centroid", "numphotos_trip")
+  
+  #what is the average distance of their trip centroids from their user centroid, for people with more than 1 trip
+  if(numtrips_overall>1){
+  centroid_dists_avg=mean(tripstats2$distance_from_centroid)
+  } else {
+  centroid_dists_avg=NA
+  }
   
   #summary
   #in this summary, the row where tripid=0 corresponds to the stats for all the photos for that user
-  # the average trip distance from centroid for tripid=0 is the distance between the photo and the centroid of the convex hull bounding the photos, 
-  # averaged across all photos
+  # the average trip distance from centroid for tripid=0 is the average distance between all the photos and the centroid of the convex hull bounding the photos, 
   # the average trip distance from centroid for all other tripids is the distance between 
-  # the centroid of the convex hull bounding the trip photos and the centroid of the convex hull bounding all the photos.
+  # the centroid of the convex hull bounding the trip photos and all the photos for that trip. 
   # the max distance from centroid is in relation to the centroid of the all photos for that trip (all photos for the user if tripid=0)
+  # distance from centroid is the distance between the centroid of the convex hull bounding the photos for a given trip and the centroid of the convex hull bounding all the photos.
+  # for tripid=0 I report the average of this value across trips, unless there is only 1 trip in which case NA is returned
+
   overallstats <- data.frame(owner=currowner, tripid=0, 
                              tripdist_km=total_trip_dist, 
                              triplength_days=totaltrip_days, 
+							 tripcentroid_distance_from_usercentroid=centroid_dists_avg,
                              maxtripdist_from_centroid=maxdistfromcentroid_km[[1]], 
                              avgtripdist_from_centroid=avgdistfromcentroid_km[[1]], 
                              numphotos_trip=numphotos_overall, numtrips=numtrips_overall)
@@ -140,6 +156,7 @@ allownerstats <- lapply(allowners, function(currowner){
   return(overallstats)
 })
 
+#Transform the list into a data.frame
 allownerstats2 <- do.call(rbind, allownerstats)
 
 #add column listing the user type
@@ -147,7 +164,9 @@ allownerstats2$usertype <- "regular"
 allownerstats2$usertype[allownerstats2$owner %in% superusers$owner] <- "superuser"
 allownerstats2$usertype[allownerstats2$owner %in% testusers$owner] <- "testuser"
 
-names(allownerstats2)[9:10] <- c("centroid_X", "centroid_Y")
+#edit names
+names(allownerstats2)[10:11] <- c("centroid_X", "centroid_Y")
 
+#save
 write.csv(allownerstats2, "tables/Flickr_user_trip_summary.csv", row.names = FALSE)
 
--- Flickr_userprofiling_2_stats.r
+++ Flickr_userprofiling_2_stats.r
@@ -13,29 +13,38 @@ ownerstats <- read.csv("tables/Flickr_user_trip_summary.csv", header=TRUE)
 statsfun <- function(data) {
  return(t(data %>% summarise(numphotos_avg=mean(numphotos_trip),
                      numphotos_med=median(numphotos_trip),
+                     numphotos_iqr=IQR(numphotos_trip),
                      travel_dist_avg_km=mean(tripdist_km), 
                      travel_dist_med_km=median(tripdist_km), 
-                     time_as_arctic_user_avg_days=mean(triplength_days), 
-                     time_as_arctic_user_med_days=median(triplength_days), 
+                     travel_dist_iqr_km=IQR(tripdist_km), 
+                     trip_length_avg_days=mean(triplength_days), #for tripid=0 this is time as a flikr user
+                     trip_length_med_days=median(triplength_days), 
+                     trip_length_iqr_days=IQR(triplength_days), 
                      max_ranging_distance_avg_km=mean(maxtripdist_from_centroid), 
                      max_ranging_distance_med_km=median(maxtripdist_from_centroid), 
+                     max_ranging_distance_iqr_km=IQR(maxtripdist_from_centroid), 
                      avg_ranging_distance_avg_km=mean(avgtripdist_from_centroid), 
-                     avg_ranging_distance_med_km=median(avgtripdist_from_centroid), 
-                     numtrips_avg=mean(numtrips), 
-                     numtrips_med=median(numtrips) 
+                     avg_ranging_distance_med_km=median(avgtripdist_from_centroid), #for tripid=0 this is the median of the user averages
+                     avg_ranging_distance_iqr_km=IQR(avgtripdist_from_centroid), 
+					 trip_distance_from_usercentroid_mean_km=mean(tripcentroid_distance_from_usercentroid, na.rm=TRUE),  
+					 trip_distance_from_usercentroid_med_km=median(tripcentroid_distance_from_usercentroid, na.rm=TRUE), #for tripid=0 this is the median of the user averages
+					 trip_distance_from_usercentroid_IQR_km=IQR(tripcentroid_distance_from_usercentroid, na.rm=TRUE),
+                     numtrips_avg=mean(numtrips, na.rm=TRUE), 
+                     numtrips_med=median(numtrips, na.rm=TRUE), 
+                     numtrips_iqr=IQR(numtrips, na.rm=TRUE) 
                      )))
 }
 
 #function to calculate the difference between groups
-ttestfun <- function(data1, data2, x) {
-  t.test(data1[,x], data2[,x], paired=FALSE)
+mwtestfun <- function(data1, data2, x) {
+  wilcox.test(data1[,x], data2[,x])
   }
 
 #select the different user groups
 superuser <- ownerstats %>% filter(tripid==0 & usertype=="superuser") #people who contribute > 50% photos
 regularuser <- ownerstats %>% filter(tripid==0 & usertype=="regular") #people who contribute between n=2 and 50% photos
 testuser <- ownerstats %>% filter(tripid==0 & usertype=="testuser") #people who contribute between n=2 and 50% photos
-alluser <- ownerstats %>% filter(tripid==0 & usertype=="regular" | usertype=="superuser") #both super & regular users, excluding people who contribute just 1 or 2 photos (we assume they are just trialling flickr)
+alluser <- ownerstats %>% filter(tripid==0 & usertype=="regular" | tripid==0 & usertype=="superuser") #both super & regular users, excluding people who contribute just 1 or 2 photos (we assume they are just trialling flickr)
 
 #select the trips for the different user groups
 superuser_trips <- ownerstats %>% filter(tripid >0 & usertype=="superuser")
@@ -46,6 +55,11 @@ userstats <- data.frame(superusers=statsfun(superuser),
                   regularusers=statsfun(regularuser), 
                   testusers=statsfun(testuser),
                   allusers=statsfun(alluser))
+
+#calculate summary stats for trips of the different groups
+usertripstats <- data.frame(superusers_pertrip=statsfun(superuser_trips), 
+                  regularusers_pertrip=statsfun(regularuser_trips))
+
 
 #are the groups statistically different?
 sink("tables/Flickr_user_trip_statistics.txt")
@@ -59,34 +73,42 @@ print("Test users : contribute only 1 or 2 photos")
 print("")
 print(userstats)
 print("")
-print("t-tests of the user groups")
+print("wilcox-tests of the user groups")
 print("Have superusers travel further in their total time in the Arctic?")
 print(paste("superuser", "regularuser", "tripdist_km", sep=", "))
-print(ttestfun(superuser, regularuser, "tripdist_km"))
+print(mwtestfun(superuser, regularuser, "tripdist_km"))
 print("Have superusers been uploading Arctic photos for longer?")
 print(paste("superuser", "regularuser", "triplength_days", sep=", "))
-print(ttestfun(superuser, regularuser, "triplength_days"))
-print("Are superusers more wide-ranging (average distance of all their photos from user centroid)?")
-print(paste("superuser", "regularuser", "avgtripdist_from_centroid", sep=", "))
-print(ttestfun(superuser, regularuser, "avgtripdist_from_centroid"))
+print(mwtestfun(superuser, regularuser, "triplength_days"))
+print("Do superusers take more trips?")
+print(paste("superuser", "regularuser", "numtrips", sep=", "))
+print(mwtestfun(superuser, regularuser, "numtrips"))
+print("Are superusers more wide-ranging (average distance of their trip centroids from user centroid)?")
+print(paste("superuser", "regularuser", "tripcentroid_distance_from_usercentroid", sep=", "))
+print(mwtestfun(superuser, regularuser, "tripcentroid_distance_from_usercentroid"))
 print("Are superusers more wide-ranging (max distance of all their photos from user centroid)?")
 print(paste("superuser", "regularuser", "maxtripdist_from_centroid", sep=", "))
-print(ttestfun(superuser, regularuser, "maxtripdist_from_centroid"))
+print(mwtestfun(superuser, regularuser, "maxtripdist_from_centroid"))
+print("Are superusers more wide-ranging (avg distance of all their photos from user centroid)?")
+print(paste("superuser", "regularuser", "avgtripdist_from_centroid", sep=", "))
+print(mwtestfun(superuser, regularuser, "avgtripdist_from_centroid"))
+print(usertripstats)
 print("Do superusers travel further per trip?")
 print(paste("superuser_trips", "regularuser_trips", "tripdist_km", sep=", "))
-print(ttestfun(superuser_trips, regularuser_trips, "tripdist_km"))
+print(mwtestfun(superuser_trips, regularuser_trips, "tripdist_km"))
 print("Do superusers contribute more photos per trip?")
-print(paste("superuser_trips", "regularuser_trips", "numphotos"))
-print(ttestfun(superuser_trips, regularuser_trips, "numphotos"))
+print(paste("superuser_trips", "regularuser_trips", "numphotos_trip"))
+print(mwtestfun(superuser_trips, regularuser_trips, "numphotos_trip"))
 print("Do superusers take longer trips?")
 print(paste("superuser_trips", "regularuser_trips", "triplength_days", sep=", "))
-print(ttestfun(superuser_trips, regularuser_trips, "triplength_days"))
-print("Are superusers more wide-ranging (distance of trip centroids from user centroid)?")
+print(mwtestfun(superuser_trips, regularuser_trips, "triplength_days"))
+print("Are superusers more wide-ranging (average distance of each photo in a trip from trip centroid)?")
 print(paste("superuser_trips", "regularuser_trips", "avgtripdist_from_centroid", sep=", "))
-print(ttestfun(superuser_trips, regularuser_trips, "avgtripdist_from_centroid"))
-print("Are superusers more wide-ranging per trip (max distance from trip centroid)?")
+print(mwtestfun(superuser_trips, regularuser_trips, "avgtripdist_from_centroid"))
+print("Are superusers more wide-ranging per trip (max distance of trip photos from trip centroid)?")
 print(paste("superuser_trips", "regularuser_trips", "maxtripdist_from_centroid", sep=", "))
-print(ttestfun(superuser_trips, regularuser_trips, "maxtripdist_from_centroid"))
+print(mwtestfun(superuser_trips, regularuser_trips, "maxtripdist_from_centroid"))
 
 sink()
+
 
