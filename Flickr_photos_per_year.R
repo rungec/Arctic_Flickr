845918c5ef77f6e3f4995e66ce88fe86f1a97bd1
rungec <runge@nceas.ucsb.edu>
2018-04-27 09:51:38 +0200

    added output paths

--- Flickr_photos_per_year.R
+++ Flickr_photos_per_year.R
@@ -1,8 +1,14 @@
-# devtools::install_github("remi-daigle/flickRgeotag")
+ devtools::install_github("remi-daigle/flickRgeotag")
 require(flickRgeotag) # must have the latest version, commited April 23, 2018
 require(ggplot2)
 require(tidyverse)
-api_key = 'd4a3326bfa4bdbfaba49ab6bd8b40bab'
+require(lubridate) 
+ 
+api_key = '790ae098b7062ef9d5f4071d0933f23c'
+
+wd <- "D:/Box Sync/Arctic/CONNECT/Paper_3_Flickr/Analysis/"
+setwd(wd)
+
 # we need to grab stats by hour, otherwise the flickr api estimate the total poorly!
 df <- expand.grid(datetime=seq(as_datetime("2000-01-01 00:00:00"), as_datetime("2018-04-23 23:59:59"), by="+1 hour"),total=0)
 for(y in df$datetime){
@@ -11,13 +17,16 @@ for(y in df$datetime){
                                                output = "total",
                                                .allpages = F,
                                                min_taken_date=as_datetime(y),
-                                               max_taken_date=as_datetime(y+60*60-1),
+                                               max_taken_date=as_datetime(y+60*60-1)
   )
 }
+
+write.csv(df, "tables/Flickr_global_nphotostaken_byhr_2000to2018.csv")
 
 #plot by hour
 ggplot(data = df,aes(x=datetime,y=total))+
   geom_line()
+ggsave("figures/Flickr_global_nphotostaken_byhr_2000to2018.png")
 
 #plot by year
 dfByYear = df %>%
@@ -27,3 +36,4 @@ dfByYear = df %>%
 
 ggplot(data = dfByYear,aes(x=year,y=total))+
   geom_line()
+ggsave("figures/Flickr_global_nphotostaken_byyr_2000to2018.png")
