91f51dbcf7dd9e24601a6f6275220c28097540b0
rungec <runge@nceas.ucsb.edu>
2018-05-04 13:03:02 +0200

    merge all datasets to make a megadataset

--- Flickr_googlecloudvision_postprocessing.r
+++ Flickr_googlecloudvision_postprocessing.r
@@ -4,6 +4,8 @@
 ### Set up libraries ----
 library(sf)
 library(tidyverse)
+library(wordcloud)
+library(extrafont)
 
 wd <- "D:/Box Sync/Arctic/CONNECT/Paper_3_Flickr/Analysis/tag_analysis"
 #wd <- "/home/cru016@ad.uit.no/Documents/tag_analysis"
@@ -19,10 +21,26 @@ regionlist <- list(IcelandGreenland=c("Iceland", "Greenland"),
                     Marine=c("Marine"), 
                     OtherIslands =c("Faroe.Islands", "United.Kingdom"))
 
+#load flickrshp
+load("input/Flickr_Artic_60N_plus_flickr_labels_urban.Rdata")
+flickrshp <- flickrshp_tags
+rm(flickrshp_tags)					
+
+########################
+#set up functions
+
+#set up wordcloud plotfun
+wordplotfun <- function(words, freq, outname, ...){
+  png(filename = sprintf("output/byregion/Google_labels_wordcloud_%s.png", outname), width=14, height=14, units="in", type='windows', antialias = "cleartype", res=600)
+  wordcloud(words=words, freq=freq, random.order=FALSE, scale=c(10,1.5), family="Times New Roman", ...)
+  dev.off()
+  }
+
 
 ### Post processing ---
 ########################
 ## Combine data ----
+########################
 #about 5.6gb total so we do it by region
 for(i in seq_along(regionlist)){
   curregion <- regionlist[i]
@@ -40,18 +58,21 @@ for(i in seq_along(regionlist)){
     }
 
 #########################
-#Summarise the data for each photo
+#Summarise the data for each photo ----
+########################
 #make a table of the top google tags & their scores 
 for(i in seq_along(regionlist)){
   curregion <- regionlist[i]
   curregname <- names(curregion)
-  allgoogle <- load(sprintf("output/byregion/Google_output_%s.Rdata", curregname))
+  load(sprintf("output/byregion/Google_output_%s.Rdata", curregname))
   
   #add a summary of the data
 	b <- list() 
 	for(d in 1:length(allgoogle)){
 			x <- allgoogle[[d]]
 			if( is.null(x[[1]]) ){
+				df_currphoto <- data.frame(numtags=NA)
+			} else if (grepl("error", x, ignore.case=TRUE))	{
 				df_currphoto <- data.frame(numtags=NA)
 			} else if (names(x[[1]][1])=="error" )	{
 				df_currphoto <- data.frame(numtags=NA)
@@ -72,7 +93,7 @@ for(i in seq_along(regionlist)){
 	}
 	summaryDF <- do.call(plyr::rbind.fill, b)
 	summaryDF <- data.frame(id=names(allgoogle), summaryDF)
-write.csv(summaryDF, sprintf("output/byregion/Google_labels_summary_foreachphoto_%s.csv", curregname), row.names = TRUE)
+write.csv(summaryDF, sprintf("output/byregion/Google_labels_summary_foreachphoto_%s.csv", curregname), row.names = TRUE, fileEncoding = "UTF-8")
 print(paste("Finished", curregname, length(which(is.na(summaryDF$numtags))), "NAs of", nrow(summaryDF), sep=" "))
 
 }
@@ -82,48 +103,54 @@ print(paste("Finished", curregname, length(which(is.na(summaryDF$numtags))), "NA
 for(i in seq_along(regionlist)){
   curregion <- regionlist[i]
   curregname <- names(curregion)
-  summaryDF <- load(sprintf("output/byregion/Google_labels_summary_foreachphoto_%s.csv", curregname))
+  summaryDF <- read.csv(sprintf("output/byregion/Google_labels_summary_foreachphoto_%s.csv", curregname), header=TRUE)
   
-  tagfreq <- plyr::count(alltags[alltags$score>=0.6, "description"]) #count how frequently each tag is used
-    write.csv(tagfreq, sprintf("output/byregion/Frequency_of_google_labels_overscore60_%s.csv", curregname), fileEncoding = "UTF-8")
-}		
+  tagfreq <- plyr::count(unlist(summaryDF[grep("googletag", names(summaryDF), value=FALSE)])) #count how frequently each tag is used
+  #drop the unlist to get the frequency by column (i.e the most frequent first word etc)
+    write.csv(tagfreq, sprintf("output/byregion/Frequency_of_google_labels_overscore60_%s.csv", curregname), fileEncoding = "UTF-8", row.names=FALSE)
+	
+	#plot wordcloud
+	wordplotfun(tagfreq$x, tagfreq$freq, outname=curregname, max.words=100, rot.per=0)
+} 
+
+#########################
+# Combine frequencies for all regions
+filelist <- list.files("output/byregion/", pattern="Frequency_of_google_labels_overscore60")
+allfreqL <- lapply(filelist, function(i) {
+			a <- read.csv(paste0("output/byregion/", i), header=TRUE, fileEncoding="UTF-8")
+			return(a)
+			})
+allfreqDF <- do.call(rbind, allfreqL)
+allfreq <- allfreqDF %>% group_by(x) %>% summarise(freq=sum(freq)) 			
+write.csv(allfreq, "output/Frequency_of_google_labels_overscore60_Arctic.csv", fileEncoding="UTF-8", row.names=FALSE)
+wordplotfun(allfreq$x, allfreq$freq, outname="Arctic", max.words=100, rot.per=0)
 
 
+#########################
+#Make a merged dataset ----
+#########################
+#contains flickr tags & titles, google labels, whether urban or not, whether superuser or not, whether tourist or local.
+#load flickrshp
+load("input/Flickr_Artic_60N_plus_flickr_labels_urban.Rdata")
+flickrshp <- flickrshp_tags
+rm(flickrshp_tags)
+
+#load userinfo
+userinfoDF <- read.csv("tables/Flickr_userinfo_tourist_or_superuser.csv", fileEncoding="UTF-8", header=TRUE)
+
+#load and combine google labels for each region
+filelist <- list.files("output/byregion/", pattern="Google_labels_summary_foreachphoto_", full.names = TRUE)
+allphotosL <- lapply(filelist, function(i) {
+			a <- read.csv(i, header=TRUE)
+			return(a)
+			})
+allphotos <- do.call(rbind, allphotosL)
+
+#Merge user info with flickrshp
+flickrshp <- merge(flickrshp, userinfoDF, by.x="owner", by.y="owner", all.x=TRUE)
+#merge google labels with flickrshp
+flickrshp <- merge(flickrshp, allphotos, by.x="id", by.y="id", all.x=TRUE)
+save(flickrshp, file="output/Flickr_Artic_60N_plus_flickrandgooglelabels_userinfo_urban.Rdata")
 
 
-  
-  
-    #Join the google data to flickrshp and save it
-    flickrshp_sub$google <- merge(flickrshp_sub, summaryDF, by.x="id", by.y="id", all.x=TRUE)
-    save(flickrshp_sub,file=sprintf("output/byregion/Flickr_Artic_60N_plus_flickr_and_google_labels_%s.Rdata", curregname))
-    
-    #test that the ids match between the two datasets
-    checkfun <- function(flickrshpt){
-      counter=0
-      for(i in 1:nrow(flickrshpt)){
-        if(flickrshpt$id[i]!=as.numeric(names(flickrshpt$google)[[i]])) print(sprintf("ID mismatch row %s original %s google %s", i, flickrshpt$id[i], names(flickrshpt$google)[[i]]))
-        else  (counter=counter+1)
-      }
-      print(sprintf("%s of %s ids match", nrow(flickrshpt), counter))
-    }
-    checkfun(flickrshp_sub)
-  
-  
-  
-  #function to save as shp
-  st_write(google_summary, sprintf("output/byregion/Flickr_Artic_60N_plus_google_labels_%s.shp", curregname))
-  google_summary_df <- google_summary %>% st_set_geometry(NULL) %>% data.frame()
-    write.csv(google_summary_df, sprintf("output/byregion/Flickr_Artic_60N_plus_top10googlelabels_%s.csv", curregname), fileEncoding = "UTF-8")
-
-
-  
-  print(paste0("FINISHED POST PROCESSING for ", curregname))
-}
-
-
-
-
-                   
-
-
-
+###END
