07d2018873f1c0d066213429e6db5e63ff91a8fc
rungec <runge@nceas.ucsb.edu>
2018-05-30 14:11:12 +0200

    now use 60N as template

--- Flickr_googlecloudvision_esmapping.r
+++ Flickr_googlecloudvision_esmapping.r
@@ -9,7 +9,10 @@ require(tidyverse)
 require(raster)
 
 wd <- "D:/Box Sync/Arctic/CONNECT/Paper_3_Flickr/Analysis/es_mapping"
+#wd <- "/data/Claire/rasters/es_mapping"
 setwd(wd)
+wd2 <- "D:/Box Sync/Arctic/Data"
+#wd2 <- "/data/Claire/rasters"
 
 
 ##########################
@@ -20,34 +23,37 @@ load("Flickr_Artic_60N_plus_flickrandgooglelabels_amap_escodes.Rdata")
 #regular and super users only, 
 #dropped rows with no google vision labels
 #includes photos in towns/cities
+
 #add a new column, owner_date
 flickramap$owner_date <- paste(flickramap$owner, flickramap$datetkn, sep="_")
 
 #load country borders shp
 #worldmap <- readOGR("D:/Box Sync/Arctic/Data/Boundaries/Arctic_circle/60degreesN/CountryBordersESRI_60degreesN_lambert.shp")
-#load AMAP boundaries - I updated the Yamal borders, and clipped out any areas south of 60N. 
-amap <- read_sf("D:/Box Sync/Arctic/Data/Boundaries/Arctic_circle/AMAP/AMAP_updatedRussia_clipto60N.shp")
-rcrs <- crs(amap)
+#load bounding box shp
+boundary60N <- readOGR(paste0(wd2, "/Boundaries/Arctic_circle/60degreesN"), "60degreesN")
+rcrs <- crs(boundary60N)
+# though we use amap boundaries in the analysis, I rasterise the data based on the maximum extent of the data
+# so that if we revise the study region I don't have to redo the rasterization.
 
 #######################
 #Function to rasterise es where the value in each cell = PUD
 #PUD = photo unit days
 #number of days in a cell when a photo was taken that represents that es
 
-rastfun <- function(es, curres, currfile) {
+rastfun <- function(data, es, curres, currfile) {
   #extract photos containing that es
-  escols <- grep("escode", names(flickramap), value=TRUE)
-  data_es <- filter_at(flickramap, escols, any_vars(. %in% es))
+  escols <- grep("escode", names(data), value=TRUE)
+  data_es <- filter_at(data, escols, any_vars(. %in% es))
   #set the blank raster  
   rasttemplate <- raster(xmn=-3335000, xmx=3335000, ymn=-3335000, ymx=3335000, res=curres, crs=rcrs)
-    if(file.exists(sprintf("D:/Box Sync/Arctic/Data/Boundaries/Arctic_circle/60degreesN/60degreesN_%sres.tif", currfile))==FALSE){ 
-      rastamap <- rasterize(amap, rasttemplate, filename=sprintf("D:/Box Sync/Arctic/Data/Boundaries/Arctic_circle/AMAP/AMAP_%sres.tif", currfile))
-    } else {
-      rastamap <- raster(sprintf("D:/Box Sync/Arctic/Data/Boundaries/Arctic_circle/AMAP/AMAP_%sres.tif", currfile))
-    }
-    amap[amap==1] <- 0
+    if(file.exists(sprintf("%s/Boundaries/Arctic_circle/60degreesN/60degreesN_%smres.tif", wd2, curres))==FALSE){ 
+        rast60N <- rasterize(boundary60N, rasttemplate, filename=sprintf("%s/Boundaries/Arctic_circle/60degreesN/60degreesN_%smres.tif", wd2, curres))
+      } else {
+        rast60N <- raster(sprintf("%s/Boundaries/Arctic_circle/60degreesN/60degreesN_%smres.tif", wd2, curres))
+      }
+      rast60N[rast60N==1] <- 0
     #fill with pud
-    esrast <- rasterize(data_es, rastamap, fun=function(x, ...){ length(unique(x))}, field="owner_date", update=TRUE, filename=sprintf("Flickr_%s_per%scell.tif", currfile, currres), overwrite=TRUE)
+    esrast <- rasterize(data_es, rasttemplate, fun=function(x, ...){ length(unique(x))}, field="owner_date", update=TRUE, filename=sprintf("Flickr_%s_per%scell.tif", currfile, currres), overwrite=TRUE)
   }
 
 #######################
@@ -57,5 +63,5 @@ rastfun <- function(es, curres, currfile) {
 
 #Biotic
 bioes <- c("biotic_fauna", "biotic_plant")
-biorast <- rastfun(bioes, 500, "biotic_all")
+biorast <- rastfun(flickramap, bioes, 1000, "biotic_all")
 
