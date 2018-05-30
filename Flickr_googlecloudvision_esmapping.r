819530720a63892875431ba6597e7d62811b86af
rungec <runge@nceas.ucsb.edu>
2018-05-30 17:04:56 +0200

    fixed the function and added function to make combined rasters

--- Flickr_googlecloudvision_esmapping.r
+++ Flickr_googlecloudvision_esmapping.r
@@ -18,7 +18,7 @@ wd2 <- "D:/Box Sync/Arctic/Data"
 ##########################
 ### Preliminary processing ----
 
-load("Flickr_Artic_60N_plus_flickrandgooglelabels_amap_escodes.Rdata")
+load("Flickr_Artic_60N_plus_flickrandgooglelabels_escodes_amap.Rdata")
 #photos from amap region, 
 #regular and super users only, 
 #dropped rows with no google vision labels
@@ -29,11 +29,9 @@ flickramap$owner_date <- paste(flickramap$owner, flickramap$datetkn, sep="_")
 
 #load country borders shp
 #worldmap <- readOGR("D:/Box Sync/Arctic/Data/Boundaries/Arctic_circle/60degreesN/CountryBordersESRI_60degreesN_lambert.shp")
-#load bounding box shp
-boundary60N <- readOGR(paste0(wd2, "/Boundaries/Arctic_circle/60degreesN"), "60degreesN")
-rcrs <- crs(boundary60N)
-# though we use amap boundaries in the analysis, I rasterise the data based on the maximum extent of the data
-# so that if we revise the study region I don't have to redo the rasterization.
+#load AMAP boundaries - I updated the Yamal borders, and clipped out any areas south of 60N. 
+amap <- read_sf(paste0(wd2, "/Boundaries/Arctic_circle/AMAP/AMAP_updatedRussia_clipto60N.shp"))
+rcrs <- crs(amap)
 
 #######################
 #Function to rasterise es where the value in each cell = PUD
@@ -45,23 +43,52 @@ rastfun <- function(data, es, curres, currfile) {
   escols <- grep("escode", names(data), value=TRUE)
   data_es <- filter_at(data, escols, any_vars(. %in% es))
   #set the blank raster  
-  rasttemplate <- raster(xmn=-3335000, xmx=3335000, ymn=-3335000, ymx=3335000, res=curres, crs=rcrs)
-    if(file.exists(sprintf("%s/Boundaries/Arctic_circle/60degreesN/60degreesN_%smres.tif", wd2, curres))==FALSE){ 
-        rast60N <- rasterize(boundary60N, rasttemplate, filename=sprintf("%s/Boundaries/Arctic_circle/60degreesN/60degreesN_%smres.tif", wd2, curres))
-      } else {
-        rast60N <- raster(sprintf("%s/Boundaries/Arctic_circle/60degreesN/60degreesN_%smres.tif", wd2, curres))
-      }
-      rast60N[rast60N==1] <- 0
-    #fill with pud
-    esrast <- rasterize(data_es, rasttemplate, fun=function(x, ...){ length(unique(x))}, field="owner_date", update=TRUE, filename=sprintf("Flickr_%s_per%scell.tif", currfile, currres), overwrite=TRUE)
+  rasttemplate <- raster(xmn=-3335000, xmx=3335000, ymn=-3335000, ymx=3335000, res=curres, crs=rcrs$proj4string)
+    if(file.exists(sprintf("%s/Boundaries/Arctic_circle/AMAP/AMAP_%smres.tif", wd2, curres))==FALSE){ 
+      rastamap <- rasterize(amap, rasttemplate, filename=sprintf("%s/Boundaries/Arctic_circle/AMAP/AMAP_%smres.tif", wd2, curres))
+    } else {
+      rastamap <- raster(sprintf("%s/Boundaries/Arctic_circle/AMAP/AMAP_%smres.tif", wd2, curres))
+    }
+    rastamap[rastamap==1] <- 0
+       #fill with pud
+    esrast <- rasterize(data_es, rastamap, fun=function(x, ...){ length(unique(x))}, field="owner_date", update=TRUE, filename=sprintf("Flickr_%s_PUDper%scell.tif", currfile, currres), overwrite=TRUE)
   }
 
 #######################
 # Apply rastfun over es
-
+escats <- unique(flickramap[, names(flickramap) %in% grep("escode", names(data), value=TRUE)])
+  
+lapply(escats, function(x) {
+  rastfun(flickramap, x, 10000, x)
+} ) 
+  
 #drop rows for biotic fauna with pet
 
-#Biotic
-bioes <- c("biotic_fauna", "biotic_plant")
-biorast <- rastfun(flickramap, bioes, 1000, "biotic_all")
+#######################
+# Combined es rasters 
+# note that becuase these words can occur in a single photo, individual rasters for es cannot simply be added together.
 
+#Biotic
+bioes <- c("biotic_fauna", "biotic_reptile", "biotic_insect", "biotic_amphibian", "biotic_plant", "biotic_fungus", "biotic_ecosystem")
+rast <- rastfun(flickramap, bioes, 10000, "biotic_all")
+
+bionature <- c("biotic_plant", "biotic_fungus", "biotic_ecosystem")
+rast <- rastfun(flickramap, bionature, 10000, "biotic_nature")
+
+biowildlife <- c("biotic_fauna", "biotic_reptile", "biotic_insect", "biotic_amphibian")
+rast <- rastfun(flickramap, bioes, 10000, "biotic_wildlife")
+
+#abiotic
+dput(escats)
+
+#harvesting and hunting
+harvest_es <- c()
+rast <- rastfun(flickramap, harvest_es, 10000, "harvest_all")
+harvestorhunt_es <- c(harvest_es, "recreation_hunting", "recreation_fishing")
+rast <- rastfun(flickramap, harvest_es, 10000, "harvesthuntingfishing")
+
+#recreation
+recreation <- c()
+rast <- rastfun(flickramap, recreation, 10000, "recreation_all")
+
+#END######################
