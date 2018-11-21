920452d9b70829ad53af2a536c7439e40771b9a6
rungec <runge@nceas.ucsb.edu>
2018-11-21 15:44:35 +0100

    trial different models

--- GAM_model_exploration.r
+++ GAM_model_exploration.r
@@ -2,11 +2,12 @@ require(tidyverse)
 require(mgcv)
 require(modelr)
 
+setwd("D:/Box Sync/Arctic/CONNECT/Paper_3_Flickr/Analysis/model/gam_10km")
 
 #gw <- readRDS(file.choose())
 #plot(gw, all.terms = TRUE)
 
-currdf <- readRDS(file.choose())
+currdf <- readRDS("D:/Box Sync/Arctic/CONNECT/Paper_3_Flickr/Analysis/model/input/gridYearPUD_models10000_m.Rdata")
 currdf <- currdf %>% filter(season=="winter") %>%
   select(-one_of("geometry")) %>%
   filter(country!="Russia") %>%
@@ -23,8 +24,15 @@ currdf <- currdf %>% filter(season=="winter") %>%
 
 subdf <- currdf %>% filter(PUD>0) %>% 
   #sample_n(100000) %>% 
+  #subdf <- subdf %>%
   mutate(PUDlog = log(PUD), 
-         PUDlog10 =log10(PUD))
+         PUDlog10 =log10(PUD), 
+         logdist2road = log(dist2road),
+         logdist2airports = log(dist2airports),
+         logdist2ports = log(dist2ports),
+         logdist2populated_places = log(dist2populated_places),
+         roadkm = roadlength/1000
+         )
 rm(currdf)
 
 #SET UP PLOT FUNCTIONS
@@ -50,59 +58,148 @@ plotmodelbyregion <- function(x, y, preds2){
 }  
 
 
-
-
-
-
-pairs(subdf[,c(2,7,8,10:21)])
+###Check correlation between variables
+pairs(subdf[,c(2,7,8,24:28)])
 
 ggplot(subdf) +
-  geom_point(aes(x=log(dist2airports), y=log(PUD)))
-
-pairs(sample_n(gridYearPUD_models[, 10:21], 10000))
-
-g1 <- gamm(PUD ~ s(year) + s(dist2road), data=subdf, method="REML")
-g2 <- gamm(PUD ~ s(year) + s(dist2road) + s(Latitude) + s(Longitude), data=subdf, method="REML")
-g3 <- gamm(PUD ~ s(year) + s(dist2road) + country + dist2airports + dist2ports + dist2populated_places, data=subdf, method="REML")
-g4 <- gamm(PUDlog10 ~ s(year) + s(dist2road) + country + s(dist2airports) + s(dist2ports) + s(dist2populated_places), data=subdf, method="REML")
-g5 <- gamm(PUDlog10 ~ s(year) + s(log(dist2road)) + country + dist2airports + dist2ports + dist2populated_places, data=subdf, method="REML")
-g6 <- gamm(PUDlog10 ~ s(year) + log(dist2road) + country + PA + dist2airports + dist2ports + dist2populated_places, data=subdf, method="REML")
-g8 <- gamm(PUDlog ~ s(year) + log(dist2road) + country + PA + log(dist2airports) + log(dist2ports) , data=subdf, method="REML")
-g9 <- gamm(PUDlog ~ s(year) + log(dist2road) + country + log(dist2airports) + log(dist2ports) + log(dist2populated_places), data=subdf, method="REML")
-g10 <- gamm(PUDlog ~ s(year) + log(dist2road) + log(dist2airports) + log(dist2ports) + log(dist2populated_places), data=subdf, method="REML")
-g11 <- gamm(PUDlog ~ s(year) + log(dist2road) + country + log(dist2airports) + log(dist2populated_places), data=subdf, method="REML")
-anova(g7$lme, g8$lme, g9$lme, g10$lme, g11$lme)
-anova(g7$lme, g9$lme) #no signif difference
+  geom_point(aes(x=logdist2airports, y=logPUD))
 
 
-#
-g7 <- gamm(PUDlog ~ s(year) + log(dist2road) + log(dist2airports) + log(dist2ports) + log(dist2populated_places) + country + PA, data=subdf, method="REML")
 
-#test autocorrelation structures
-gar0 <- gamm(PUDlog ~  s(year) + log(dist2road) + log(dist2airports) + log(dist2ports) + log(dist2populated_places) + PA, random=list(country=~1), correlation=NULL, data=subdf, method="REML")
-gar1 <- gamm(PUDlog ~  s(year) + log(dist2road) + log(dist2airports) + log(dist2ports) + log(dist2populated_places) + PA, random=list(country=~1), correlation = corARMA(form = ~ 1|year, p=1), data=subdf, method="REML")
-gma2 <- gamm(PUDlog ~  s(year) + log(dist2road) + log(dist2airports) + log(dist2ports) + log(dist2populated_places) + PA, random=list(country=~1), correlation = corARMA(form = ~ 1|year, p=1, q=2), data=subdf, method="REML")
+# The full model
+g0 <- gamm(PUDlog ~ s(year) + logdist2road + logdist2airports + logdist2ports + logdist2populated_places + roadkm + PA, 
+           random=list(country=~1), 
+           data=subdf, method="REML")
+
+# The full model with smooths
+g1 <- gamm(PUDlog ~  s(year) + s(logdist2road) + s(logdist2airports) + s(logdist2ports) + s(logdist2populated_places) + s(roadkm) + PA, 
+           random=list(country=~1), 
+           data=subdf, method="REML")
+
+# The full model with only smoothed airport, roadlength and year
+g2 <- gamm(PUDlog ~  s(year) + logdist2road + s(logdist2airports) + logdist2ports + logdist2populated_places + s(roadkm) + PA, 
+                 random=list(country=~1), 
+                 data=subdf, method="REML")
+
+
+anova(g0$lme, g1$lme, g2$lme)
+
+
+par(mfrow=c(1,2))
+plot(g0$gam)
+sink("GAM_model0_summary.txt")
+print(summary(g0$gam))
+par(mfrow=c(2,2))
+print(gam.check(g0$gam))
+sink()
+
+par(mfrow=c(2,3))
+plot(g1$gam)
+sink("GAM_model1_summary.txt")
+print(summary(g1$gam))
+par(mfrow=c(2,2))
+print(gam.check(g1$gam))
+sink()
+
+par(mfrow=c(2,2))
+plot(g2$gam)
+sink("GAM_model2_summary.txt")
+print(summary(g2$gam))
+par(mfrow=c(2,2))
+print(gam.check(g2$gam))
+sink()
+
+par(mfrow=c(1,2))
+acf(resid(g2$lme, type="normalized"), main="ACF")
+pacf(resid(g2$lme, type="normalized"), main="PACF")
+
+
+#test temporal autocorrelation structures
+gar0 <- gamm(PUDlog ~  s(year) + logdist2road + s(roadkm) + s(logdist2airports) + logdist2ports + logdist2populated_places + PA, 
+             random=list(country=~1), 
+             correlation=NULL, 
+             data=subdf, method="REML")
+gar1 <- gamm(PUDlog ~  s(year) + logdist2road + s(roadkm) + s(logdist2airports) + logdist2ports + logdist2populated_places + PA, 
+             random=list(country=~1), 
+             correlation = corARMA(form = ~ 1|year, p=1), 
+             data=subdf, method="REML")
+gma2 <- gamm(PUDlog ~  s(year) + logdist2road + s(roadkm) + s(logdist2airports) + logdist2ports + logdist2populated_places + PA, 
+             random=list(country=~1), 
+             correlation = corARMA(form = ~ 1|year, p=1, q=2), 
+             data=subdf, method="REML")
 
 anova(gar0$lme, gar1$lme, gma2$lme)
 
+par(mfrow=c(2,2))
+plot(gma2$gam)
+sink("GAM_model2_ma2_summary.txt")
+print(summary(gma2$gam))
+par(mfrow=c(2,2))
+print(gam.check(gma2$gam))
+sink()
 
-AIC(g7b$lme)
-summary(g7b$gam)
-par(mfrow=c(1,1))
-gam.check(g7b$gam)
+par(mfrow=c(2,2))
+acf(resid(g2$lme, type="normalized"), main="ACF")
+pacf(resid(g2$lme, type="normalized"), main="PACF")
+acf(resid(gma2$lme, type="normalized"), main="ACF")
+pacf(resid(gma2$lme, type="normalized"), main="PACF")
 
-acf(resid(gar0$lme, type="normalized"), main="ACF")
-pacf(resid(gar0$lme, type="normalized"), main="PACF")
 
-plot(g4$gam)
+#try more parsimonious models
+# Drop PA
+g3 <- gamm(PUDlog ~  s(year) + logdist2road + s(logdist2airports) + logdist2ports + logdist2populated_places + s(roadkm), 
+           random=list(country=~1), 
+           correlation = corARMA(form = ~ 1|year, p=1, q=2),
+           data=subdf, method="REML")
+# Drop Populated places and PA
+g4 <- gamm(PUDlog ~  s(year) + logdist2road + s(logdist2airports) + logdist2ports + s(roadkm), 
+           random=list(country=~1),
+           correlation = corARMA(form = ~ 1|year, p=1, q=2),
+           data=subdf, method="REML")
+# Drop dist2roads and PA
+g5 <- gamm(PUDlog ~  s(year) + s(logdist2airports) + logdist2ports + logdist2populated_places + s(roadkm), 
+           random=list(country=~1), 
+           correlation = corARMA(form = ~ 1|year, p=1, q=2),
+           data=subdf, method="REML")
+# Drop dist2roads
+g6 <- gamm(PUDlog ~  s(year) + s(logdist2airports) + logdist2ports + logdist2populated_places + s(roadkm) + PA, 
+           random=list(country=~1), 
+           correlation = corARMA(form = ~ 1|year, p=1, q=2),
+           data=subdf, method="REML")
+# Drop dist2roads and PA and logdist2ports
+g7 <- gamm(PUDlog ~  s(year) + s(logdist2airports) + logdist2populated_places + s(roadkm), 
+           random=list(country=~1), 
+           correlation = corARMA(form = ~ 1|year, p=1, q=2),
+           data=subdf, method="REML")
+anova(gma2$lme, g3$lme) #no significant difference between models
+anova(gma2$lme, g4$lme) #model 2 significantly better than model 4
+anova(gma2$lme, g5$lme) #small but not very significant difference between models
+anova(gma2$lme, g6$lme) #small but not very significant difference between models
+anova(gma2$lme, g7$lme) #small but not very significant difference between models
 
-par(mfrow=c(1,2))
-plot(g3$gam)
+par(mfrow=c(2,2))
+plot(g5$gam)
+sink("GAM_model5_ma2_summary.txt")
+print(summary(g5$gam))
+par(mfrow=c(2,2))
+print(gam.check(g5$gam))
+sink()
 
-par(mfrow=c(1,2))
-acf(resid(g1$lme, type="normalized"), lag=20, main="ACF")
-pacf(resid(g1$lme, type="normalized"), lag=20, main="PACF")
 
+#check spatial autocorrelation using 2016 data
+gsc <- gamm(PUDlog ~ logdist2road + s(roadkm) + s(logdist2airports) + logdist2ports + logdist2populated_places + PA, random=list(country=~1), correlation = corRatio(form = ~ Latitude + Longitude, nugget=TRUE), data=subdf[subdf$year==2016,], method="REML")
+V1<- Variogram(gsc$lme, form=~Latitude+Longitude, nugget=TRUE, data=subdf[subdf$year==2016,])
+plot(V1, smooth=FALSE)
+
+dists <- dist(subdf[,7:8])
+summary(dists)
+d0 <- which(dists==0) #some are zero because we have temporal replicates
+
+
+#vis.gam
+
+
+### Make marginal plots
 newdf <- subdf %>% mutate(dist2road=seq(0, 1, length.out = 10000), 
                     Latitude=mean(Latitude), 
                     Longitude=mean(Longitude),
